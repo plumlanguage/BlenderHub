24384b983a8c41e7d23cf527a80e5ff013d742bb
KinichJueMe <aas0309@163.com>
2025-03-28 13:56:10 +0800

    初始化

---
+++ main.js
@@ -0,0 +1,11 @@
+﻿const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+
+  });
+
+  win.loadFile('index.html')
+}
